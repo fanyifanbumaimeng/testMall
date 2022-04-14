e76c5bfc74d669008026bc281fb256e3efc967f3
fyfan <fyf171795@alibaba-inc.com>
2022-04-14 23:06:13 +0800

    Updates

---
+++ precache-manifest.4be58a2b5df5f3687f7d372450c2cb90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "184b62aaf3a606fff279",
+    "url": "/static/css/main.f5b119fe.chunk.css"
+  },
+  {
+    "revision": "184b62aaf3a606fff279",
+    "url": "/static/js/main.184b62aa.chunk.js"
+  },
+  {
+    "revision": "bff5e4150716f03899c0",
+    "url": "/static/css/1.0315b2d3.chunk.css"
+  },
+  {
+    "revision": "bff5e4150716f03899c0",
+    "url": "/static/js/1.bff5e415.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bda157d154ced76461f456745baa7c48",
+    "url": "/static/media/select_1.bda157d1.png"
+  },
+  {
+    "revision": "c2496279d69279f9812dcb95297a07c9",
+    "url": "/static/media/select_2.c2496279.png"
+  },
+  {
+    "revision": "39e117f6eb6762a30248e60c82098dc9",
+    "url": "/static/media/relevant_2.39e117f6.png"
+  },
+  {
+    "revision": "8a6574d15d4c97ee31d48cd074321764",
+    "url": "/static/media/relevant_3.8a6574d1.png"
+  },
+  {
+    "revision": "926e42cfe8f1b8bbce48d491bbfcdfe2",
+    "url": "/static/media/alipay.926e42cf.jpg"
+  },
+  {
+    "revision": "9091f52c1f5949eae740607f66c297a8",
+    "url": "/static/media/weixing.9091f52c.jpg"
+  },
+  {
+    "revision": "86fbf83c21d80c2112adca949ce142c1",
+    "url": "/index.html"
+  }
+];
